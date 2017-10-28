3fbd5b264e18f41a779973a77efe51be876f1ac9
Ellmen <isaac.ellmen@thescore.com>
2017-10-28 17:09:53 -0400

    Style cleanup

--- models/aggregation/SplitYFPamyloids.py
+++ models/aggregation/SplitYFPamyloids.py
@@ -4,46 +4,41 @@ import math
 import matplotlib.pyplot as plt
 #The binomial coefficient nCk...
 def binom(n, k):
-	if k > n or k < 0:
-		return 0
-	a = math.factorial(n)
-	b = math.factorial(k)
-	c = math.factorial(n - k)
-	return a / (b * c)
+    if k > n or k < 0:
+	return 0
+    a = math.factorial(n)
+    b = math.factorial(k)
+    c = math.factorial(n - k)
+    return a / (b * c)
 
 #This is the \#(n,k) function I defined in my writeup...
 def num(n, k):
-	return binom(n - k, k)
+    return binom(n - k, k)
 
 #This is the A(n,k) function I defined in my writeup...
 def A(n, k):
-	return pow(2, k) * num(n - 2,k - 1) + pow(2, k + 1) * num(n - 1, k)
+    return pow(2, k) * num(n - 2,k - 1) + pow(2, k + 1) * num(n - 1, k)
 
 #The expected number of fluorescing locations in a length n
 #amyloid, according to formula I derived, is:
 def expected_value(length):
-	result = 0.0
-	for i in range(0, int(length / 2)):
-		result += i * A(length, i)
+    result = 0.0
+    for i in range(0, int(length / 2)):
+        result += i * A(length, i)
 
-	result /= pow(2, length)
-	return result
-
-
-
+    result /= pow(2, length)
+    return result
 
 if __name__ == '__main__':
-	data = []
-		
-	print "Ratio of expected fluorescing proteins  on an amyloid of length {}: {}"
-	for i in range(2, 1000):
-		r = 2 * expected_value(i) / i 
-		print "{}: {}".format(i, r)
-		data.append(r)
+    data = []
+    print "Ratio of expected fluorescing proteins  on an amyloid of length {}: {}"
+    for i in range(2, 1000):
+        r = 2 * expected_value(i) / i 
+        print "{}: {}".format(i, r)
+        data.append(r)
 
 
 fig1 = plt.figure()
-
   
 l = plt.plot(range(len(data)), data, 'r-')
 
--- models/aggregation/Sup35amyloids.py
+++ models/aggregation/Sup35amyloids.py
@@ -1,79 +1,59 @@
 #!/usr/bin/python
 import itertools
 import math
-#import matplotlib.pyplot as plt
+
 #The binomial coefficient nCk...
 def binom(n, k):
-	if k > n or k < 0:
-		return 0
-	a = math.factorial(n)
-	b = math.factorial(k)
-	c = math.factorial(n - k)
-	return a / (b * c)
-
+    if k > n or k < 0:
+        return 0
+    a = math.factorial(n)
+    b = math.factorial(k)
+    c = math.factorial(n - k)
+    return a / (b * c)
 
 def a(n):
-	if n == 0:
-		return 0
-	elif n > 0:
-		x = math.sqrt(2)
-		
-		result = ((3+2*x) /(x + 2) * 1.0) * pow((1.0 / (-1.0 - x)),n) + ((2*x - 3)/(x-2) * 1.0) * pow((1.0 / (x - 1)),n)
-		return result
+    if n == 0:
+        return 0
+    x = math.sqrt(2)
+    result = ((3 + 2 * x) / (x + 2) * 1.0) * pow((1.0 / (-1.0 - x)), n) + ((2 * x - 3) / (x - 2) * 1.0) * pow((1.0 / (x - 1)), n)
+    return result
 
 def b(n):
-	x = math.sqrt(2)
+    x = math.sqrt(2)
+    result = (1.0 / (2 * x + 2)) * pow((1.0 / (-1 - x)), n) + (1.0 / (2 - 2 * x)) * pow((1.0 / (x - 1)), n)
+    return -1 * result
 
-	result = (1.0/(2 * x + 2)) * pow((1.0/(-1 - x)),n) + (1.0/(2 - 2*x)) * pow((1.0/(x - 1)),n)
-	return -1 * result
-
-		
+def product(lst):
+    val = 1.0
+    for item in lst:
+        val *= item
+    return val
 
 #This is the A(n,k) function I defined in my writeup...
 def A(n, k):
-	result = 0.0
-	for q in xrange(0, n + 1 - 2 * k):
- 			
-		lists = itertools.product(range(2,n-q-2 * (k - 1)), repeat=k)
-		Lists = [x for x in lists if sum(x) == n - q]
-	
-		innerSum = 0.0
-		for x in Lists:		
-			innerProduct = 1.0
-			for phi_i in x:
-				innerProduct *= a(phi_i)
-			innerSum += innerProduct	
-		result +=	b(q) * innerSum
-					
-				
-	return result	
+    result = 0.0
+    for q in xrange(0, n + 1 - 2 * k):
+        all_lists = itertools.product(range(2, n - q - 2 * (k - 1)), repeat=k)
+        lists_of_sum_n = [lst for lst in all_lists if sum(lst) == n - q]
+        innerSum = 0.0
+        for lst in lists_of_sum_n:
+            a_lst = [a(phi_i) for phi_i in lst]
+            innerSum += product(a_lst)
+        result += b(q) * innerSum
+
+    return result
+
 #The expected number of fluorescing locations in a length n
 #amyloid, according to formula I derived, is:
 def expected_value(length):
-	result = 0.0
-	for i in xrange(0, int(length / 2) + 1):
-		result += i * A(length, i)
-
-	result /= pow(3, length)
-	return result
-
-
-
+    result = 0.0
+    for i in xrange(0, length / 2 + 1):
+        result += i * A(length, i)
+    result /= pow(3, length)
+    return result
 
 if __name__ == '__main__':
-	for i in xrange(2, 1000):
-		r = 2 * expected_value(i) / i 
-		print "{}: {}".format(i, r)
+    for i in xrange(2, 1000):
+        r = 2 * expected_value(i) / i
+        print "{}: {}".format(i, r)
 
-
-#fig1 = plt.figure()
-
-#l = plt.plot(range(len(data)), data, 'r-')
-
-#plt.xlim(0, len(data))
-#plt.ylim(0, max(data) * 1.2)
-#plt.xlabel('x')
-#plt.ylabel('y')
-#plt.title('SCIENCE')
-
-#plt.show()
